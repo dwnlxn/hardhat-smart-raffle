199024efdd3faf451f5b391a1658ccb395fab2ba
ndrewnlxn <ndrewnlxn@gmail.com>
2022-11-02 16:34:43 -0400

    completed

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,66 +1,68 @@
 const { network, ethers } = require("hardhat")
-const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const {
+  networkConfig,
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("15")
+const FUND_AMOUNT = ethers.utils.parseEther("1") // 1 Ether, or 1e18 (10^18) Wei
 
-module.exports = async function ({ getNamedAccounts, deployments }) {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+  let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
 
-    // log(`chainId is ${chainId}`)
-    // log(`vrfCoordinatorV2Address is ${networkConfig[chainId]["vrfCoordinatorV2"]}`)
-    // log(`entranceFee is ${networkConfig[chainId]["entranceFee"]}`)
-    // log(`gasLane is ${networkConfig[chainId]["gasLane"]}`)
-    // log(`subscriptionId is ${networkConfig["chainId"]["subscriptionId"]}`)
-    // log(`callbackGasLimit is ${networkConfig[chainId]["callbackGasLimit"]}`)
-    // log(`interval is ${networkConfig[chainId]["interval"]}`)
+  if (developmentChains.includes(network.name)) {
+    // create VRFV2 Subscription
+    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait()
+    subscriptionId = transactionReceipt.events[0].args.subId
+    // Fund the subscription
+    // Our mock makes it so we don't actually have to worry about sending fund
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    subscriptionId = networkConfig[chainId]["subscriptionId"]
+  }
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
 
-    let vrfCoordinatorV2Address, vrfCoordinatorV2Mock
+  log("----------------------------------------------------")
+  const arguments = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[chainId]["gasLane"],
+    networkConfig[chainId]["keepersUpdateInterval"],
+    networkConfig[chainId]["raffleEntranceFee"],
+    networkConfig[chainId]["callbackGasLimit"],
+  ]
+  const raffle = await deploy("Raffle", {
+    from: deployer,
+    args: arguments,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  })
 
-    if (developmentChains.includes(network.name)) {
-        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
-        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
-        const transactionReceipt = await transactionResponse.wait(1)
-        subscriptionId = transactionReceipt.events[0].args.subId
-        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
-    } else {
-        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
-        subscriptionId = networkConfig["chainId"]["subscriptionId"]
-    }
+  if (developmentChains.includes(network.name)) {
+    await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+    log("Consumer is added")
+  }
 
-    const entranceFee = networkConfig[chainId]["entranceFee"]
-    const gasLane = networkConfig[chainId]["gasLane"]
-    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
-    const interval = networkConfig[chainId]["interval"]
-    const args = [
-        vrfCoordinatorV2Address,
-        entranceFee,
-        gasLane,
-        subscriptionId,
-        callbackGasLimit,
-        interval,
-    ]
+  // Verify the deployment
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...")
+    await verify(raffle.address, arguments)
+  }
 
-    const raffle = await deploy("Raffle", {
-        from: deployer,
-        args: args,
-        log: true,
-        waitConfirmations: network.config.blockConfirmations || 1,
-    })
-
-    if (developmentChains.includes(network.name)) {
-        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
-        log("Consumer is added")
-    }
-
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying...")
-        await verify(raffle.address, args)
-    }
-    log("----------------------------------------------------")
+  log("Enter lottery with command:")
+  const networkName = network.name == "hardhat" ? "localhost" : network.name
+  log(`yarn hardhat run scripts/enterRaffle.js --network ${networkName}`)
+  log("----------------------------------------------------")
 }
 
 module.exports.tags = ["all", "raffle"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ require("dotenv").config()
 //     process.env.ALCHEMY_MAINNET_RPC_URL ||
 //     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+  process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 // const POLYGON_MAINNET_RPC_URL =
 //     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0x"
@@ -28,86 +28,86 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API k
 const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
-            chainId: 31337,
-        },
-        localhost: {
-            chainId: 31337,
-        },
-        goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
-            saveDeployments: true,
-            chainId: 5,
-            blockConfirmations: 1,
-        },
-        // mainnet: {
-        //     url: MAINNET_RPC_URL,
-        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-        //     //   accounts: {
-        //     //     mnemonic: MNEMONIC,
-        //     //   },
-        //     saveDeployments: true,
-        //     chainId: 1,
-        // },
-        // polygon: {
-        //     url: POLYGON_MAINNET_RPC_URL,
-        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-        //     saveDeployments: true,
-        //     chainId: 137,
-        // },
-        // },
-        // etherscan: {
-        //     // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-        //     apiKey: {
-        //         goerli: ETHERSCAN_API_KEY,
-        //         polygon: POLYGONSCAN_API_KEY,
-        //     },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // }
+      chainId: 31337,
     },
-    gasReporter: {
-        //enabled: REPORT_GAS,
-        enabled: false,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    localhost: {
+      chainId: 31337,
     },
-    // contractSizer: {
-    //     runOnCompile: false,
-    //     only: ["Raffle"],
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 5,
+      blockConfirmations: 1,
+    },
+    // mainnet: {
+    //     url: MAINNET_RPC_URL,
+    //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    //     //   accounts: {
+    //     //     mnemonic: MNEMONIC,
+    //     //   },
+    //     saveDeployments: true,
+    //     chainId: 1,
     // },
-    namedAccounts: {
-        deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
-        player: {
-            default: 1,
-        },
+    // polygon: {
+    //     url: POLYGON_MAINNET_RPC_URL,
+    //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    //     saveDeployments: true,
+    //     chainId: 137,
+    // },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      // polygon: POLYGONSCAN_API_KEY,
     },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.7",
-            },
-            {
-                version: "0.8.8",
-            },
-            {
-                version: "0.4.24",
-            },
-        ],
+  },
+  gasReporter: {
+    //enabled: REPORT_GAS,
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  // contractSizer: {
+  //     runOnCompile: false,
+  //     only: ["Raffle"],
+  // },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
-    mocha: {
-        timeout: 500000, // 500 seconds max for running tests
+    player: {
+      default: 1,
     },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 3000000, // 500 seconds max for running tests
+  },
 }
